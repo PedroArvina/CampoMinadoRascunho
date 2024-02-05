b0f9caa8e5c4a03ff209afa65dd10cb6faf4cf21
Pedro AV <97904358+PedroArvina@users.noreply.github.com>
2024-02-05 10:51:44 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,9 +1,35 @@
-package campominado12;
+package campominado.gui;
+import campominado12.CampoMinado;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class Minesweeper {
-	public class App {
-	    public static void main(String[] args) throws Exception {
-	        Minesweeper minesweeper = new Minesweeper();
-	    }
-	}
+public class App {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Menu Campo Minado");
+        JButton playButton = new JButton("Jogar");
+        JButton exitButton = new JButton("Sair");
+
+        playButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new CampoMinado();
+            }
+        });
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+        frame.add(playButton);
+        frame.add(exitButton);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
