aa4472c0f765fc324de00343a5eca40201c94fb9
Pedro AV <97904358+PedroArvina@users.noreply.github.com>
2024-02-06 10:12:19 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,7 @@
 package campominado.gui;
 import campominado12.CampoMinado;
+import campominado12.CampoMinadoMedio;
+import campominado12.CampoMinadoFacil;
 import campominado12.CampoMinadoMaluco;
 import javax.swing.*;
 import java.awt.event.*;
@@ -7,7 +9,9 @@ import java.awt.event.*;
 public class App {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Menu Campo Minado");
-        JButton playButton = new JButton("Jogar");
+        JButton playButton = new JButton("Jogar Difícil");
+        JButton playButton2 = new JButton("Jogar Médio");
+        JButton playButton3 = new JButton("Jogar Fácil");
         JButton playCrazyButton = new JButton("Jogar Campo Minado Maluco");
         JButton exitButton = new JButton("Sair");
 
@@ -15,7 +19,17 @@ public class App {
             frame.dispose();
             new CampoMinado();
         });
-
+        
+        playButton2.addActionListener(e -> {
+            frame.dispose();
+            new CampoMinadoMedio();
+        });
+        
+        playButton3.addActionListener(e -> {
+            frame.dispose();
+            new CampoMinadoFacil();
+        });
+        
         playCrazyButton.addActionListener(e -> {
             frame.dispose();
             new CampoMinadoMaluco();
@@ -25,6 +39,8 @@ public class App {
 
         frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
         frame.add(playButton);
+        frame.add(playButton2);
+        frame.add(playButton3);
         frame.add(playCrazyButton);
         frame.add(exitButton);
         frame.pack();
