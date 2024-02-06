8f5ef378bb91cdb25a9c3fdd0ef3ad6cb8c92c40
Pedro AV <97904358+PedroArvina@users.noreply.github.com>
2024-02-06 10:02:55 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 package campominado.gui;
 import campominado12.CampoMinado;
+import campominado12.CampoMinadoMaluco;
 import javax.swing.*;
 import java.awt.event.*;
 
@@ -7,25 +8,24 @@ public class App {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Menu Campo Minado");
         JButton playButton = new JButton("Jogar");
+        JButton playCrazyButton = new JButton("Jogar Campo Minado Maluco");
         JButton exitButton = new JButton("Sair");
 
-        playButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-                new CampoMinado();
-            }
+        playButton.addActionListener(e -> {
+            frame.dispose();
+            new CampoMinado();
         });
 
-        exitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
+        playCrazyButton.addActionListener(e -> {
+            frame.dispose();
+            new CampoMinadoMaluco();
         });
+
+        exitButton.addActionListener(e -> System.exit(0));
 
         frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
         frame.add(playButton);
+        frame.add(playCrazyButton);
         frame.add(exitButton);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
